6fc65f5c591d3b4942b90fea6358f28e03e8921f
Benny Wong <benny@bwong.net>
2011-11-11 12:53:25 -0500

    Preserve inline <style>

--- lib/action_mailer/inline_css_hook.rb
+++ lib/action_mailer/inline_css_hook.rb
@@ -7,12 +7,16 @@ module ActionMailer
       if html_part = (message.html_part || (message.content_type =~ /text\/html/ && message))
         # Generate an email with all CSS inlined (access CSS a FS path)
         premailer = ::Premailer.new(html_part.body.to_s,
-                                    :with_html_string => true)
+                                    :with_html_string => true,
+                                    :include_style_tags => false,
+                                    :preserve_styles => true)
 
         # Prepend host to remaning URIs.
         # Two-phase conversion to avoid request deadlock from dev. server (Issue #4)
         premailer = ::Premailer.new(premailer.to_inline_css,
                                       :with_html_string => true,
+                                      :include_style_tags => false,
+                                      :preserve_styles => true,
                                       :base_url => message.header[:host].to_s)
         existing_text_part = message.text_part && message.text_part.body.to_s
         # Reset the body
